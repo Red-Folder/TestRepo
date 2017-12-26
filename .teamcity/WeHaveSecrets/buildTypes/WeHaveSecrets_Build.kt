5e1f3bdc1134d4eb2ea98611e4d86cb243ed6f9e
mark <mark@TeamCity>
2017-12-26 10:58:00 +0000

    TeamCity change in 'WeHaveSecrets' project: Synchronization with own VCS root is enabled

--- .teamcity/WeHaveSecrets/buildTypes/WeHaveSecrets_Build.kt
+++ .teamcity/WeHaveSecrets/buildTypes/WeHaveSecrets_Build.kt
@@ -1,9 +1,11 @@
 package WeHaveSecrets.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
-import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.dotnetBuild
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.dockerBuild
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.dotnetPublish
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.dotnetRestore
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.dotnetTest
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.vcs
 
 object WeHaveSecrets_Build : BuildType({
@@ -18,12 +20,12 @@ object WeHaveSecrets_Build : BuildType({
 
     steps {
         dotnetRestore {
+            name = "Restore"
             projects = "WeHaveSecrets.sln"
-        }
-        dotnetBuild {
-            projects = "WeHaveSecrets.sln"
+            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
         }
         dotnetTest {
+            name = "Test"
             projects = "WeHaveSecrets.Tests.Unit/WeHaveSecrets.Tests.Unit.csproj"
             param("dotNetCoverage.dotCover.filters", """
                 +:WeHaveSecrets
@@ -31,6 +33,28 @@ object WeHaveSecrets_Build : BuildType({
                 -:*.Tests.*
             """.trimIndent())
             param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        }
+        dotnetPublish {
+            name = "Publish"
+            projects = "WeHaveSecrets.sln"
+            configuration = "Release"
+            outputDir = "obj/Docker/publish"
+            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        }
+        dockerBuild {
+            name = "Build wehavesecrets image"
+            source = path {
+                path = "WeHaveSecrets/Dockerfile"
+            }
+            contextDir = "WeHaveSecrets"
+            namesAndTags = "wehavesecrets"
+        }
+        script {
+            name = "Start WeHaveSecrets.com"
+            scriptContent = """
+                docker rm -f wehavesecrets
+                docker run -d --network wehavesecrets_secrets-network --name wehavesecrets -e "ConnectionStrings:DefaultConnection"="Server=172.20.0.22;Database=WeHaveSecrets;User Id=sa;Password=AbC!23hgAAA;" -p 8080:80 wehavesecrets:latest
+            """.trimIndent()
         }
     }
 
